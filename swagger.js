48e45e6407044a17a313f3b4c7282b0be6bb709b
Ederson Borges <edersonh.a.b@gmail.com>
2024-11-13 10:42:54 -0300

    chore: teste doc

--- swagger.js
+++ swagger.js
@@ -1,7 +1,7 @@
 const swaggerAutogen = require('swagger-autogen')();
 
 const outputFile = './swagger_output.json';
-const endpointsFiles = ['./src/routes.ts']; // Altere para o caminho correto das suas rotas
+const endpointsFiles = ['./src/routes.ts']; // Inclua aqui os arquivos com as anotações
 
 swaggerAutogen(outputFile, endpointsFiles).then(() => {
   console.log('Swagger JSON generated');
